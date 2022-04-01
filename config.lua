d88a858a2c3d92658dd98854a7ba5549e7ce1565
inclyc <axoford@icloud.com>
2022-04-01 14:34:54 +0800

    对LaTeX显式声明

--- config.lua
+++ config.lua
@@ -22,7 +22,7 @@ lvim.builtin.project.active = true
 
 vim.g.copilot_no_tab_map = 1
 vim.g.copilot_assume_mapped = 1
-
+vim.g.tex_flavor = 'latex'
 
 vim.opt.relativenumber = true
 lvim.builtin.treesitter.ensure_installed = {
